8b1441a2018c049f3135adf81042aee8cd00bdb3
Helambe <93730827+Helambe@users.noreply.github.com>
2023-12-19 19:13:18 +0530

    Add files via upload
    i have uploaded my files

---
+++ Bank Mangement System/src/bank/mangement/system/Conn.java
@@ -0,0 +1,19 @@
+
+package bank.mangement.system;
+import java.sql.*;
+
+public class Conn {
+     Connection c;
+     Statement s;
+    public Conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        c=DriverManager.getConnection("jdbc:mysql://localhost:3306/bankmanagementsystem","root","Mysql2002" );
+        s=c.createStatement();
+        }catch(Exception e){
+            System.out.println(e);
+                    
+        }
+            
+    }
+}
